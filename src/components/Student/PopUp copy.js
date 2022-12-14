8c5743136f43fe2af6f347fa731ad1b2b03717ca
mvs1903 <101926677+mvs1903@users.noreply.github.com>
2022-12-14 15:56:23 +0530

    created popupcopy

---
+++ src/components/Student/PopUp copy.js
@@ -0,0 +1,93 @@
+import { doc, updateDoc , getDocs ,collection,getDoc } from "firebase/firestore";
+import React, {useState,useEffect} from "react";
+import { Link, useNavigate ,useParams} from "react-router-dom";
+// import {compName} from "../Admin/Notification";
+import { db } from "../firebaseConfig";
+import { useUserAuth } from "../userAuthContext";
+export default function () {
+
+  const { id } = useParams();
+
+  const [notificaton, setNotification] = useState([]);
+  const [details,setDetails]=useState({});
+
+    const [enteredSAP, setEnteredSAP] = useState(0)
+  const handleSubmit = async (e) => {
+    console.log(enteredSAP);
+    
+    // e.preventDefault()
+    // db.connection("Compny")
+    // const Compnydoc = doc(db,"Compny","TCS")
+    // const newRef = firebase.firestore("Details/"+"6003200176")
+    // await  updateDoc(Compnydoc,newRef)
+    // alert("You have Successfully Registered !");
+
+  };
+  const { logOut, user } = useUserAuth();
+  const navigate = useNavigate();
+
+  const handleNotInterested = async () => {
+    try {
+      // await logOut();
+    } catch (error) {
+      console.log(error.message);
+    }
+    alert("Your response have been succesfully recorded!")
+    // navigate("/");
+  };
+  const handleLogout = ()=>{
+    navigate("/")
+  }
+
+  const handleClick = async () => {
+    // alert("You have successfully registered");
+
+    // setDoc(doc(db,`${details['compName']}`,))
+    navigate("/Otp");
+
+    
+  };
+
+  useEffect(() => {
+
+    
+    const getCompDetails = async() =>{
+
+        
+      let details1 =  await (await getDoc(doc(db,'CompDetails',id))).data()
+      // let records = details.docs.map((doc)=>({...doc.data(),id:doc.id}))
+      setDetails(details1);
+
+
+      // setNotification(records)
+      console.log(details)
+
+    }
+    getCompDetails()
+  }, [])
+  return (
+    <>
+      <div>
+        <button className="reset" onClick={handleLogout}>
+          Log out
+        </button>
+        <form onSubmit={()=>{}}>
+          {/* <Notification/> */}
+          <h3 id="phoneVeri">
+            {" "}
+            {details['compName']} is coming for interview do you want to Register ?
+            
+          </h3>
+          <br />
+          <input type="number" className="labelIn" value={enteredSAP} onChange={(val)=>{
+            // console.log(val.target.value);
+            setEnteredSAP(val.target.value)}}/> <br />
+          <input type={'button'}  className="login" onClick={handleSubmit} value={"Interested"} />
+          <button className="reset" onClick={handleNotInterested} >
+            Not Interested
+          </button>
+        </form>
+      </div>
+    </>
+  );
+}
